27f9a201679593c058b4b7e3e07a87492c671ecc
stellapark401 <stellapark401@gmail.com>
2021-05-18 18:03:45 +0900

    solved till Q265

---
+++ encapsulation/stock.py
@@ -0,0 +1,16 @@
+class Stock:
+    def __init__(self, name, code):
+        self.name = name
+        self.code = code
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_code(self, code):
+        self.code = code
+
+    def get_name(self):
+        return self.name
+
+    def get_code(self):
+        return self.code
